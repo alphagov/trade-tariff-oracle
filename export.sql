5f78b93fb616cb858a40ca86e51b5d8f45962b42
James Abley <james.abley@gmail.com>
2013-01-23 09:53:37 +0000

    Export other tables
    Required for 2nd and 3rd quantities?

--- export.sql
+++ export.sql
@@ -1,6 +1,8 @@
 -- Script which exports the desired tables from the Oracle database into CSV format.
 -- Add table names to this as required.
+exec dump_table_to_csv('COMM');
 exec dump_table_to_csv('MFCM');
 exec dump_table_to_csv('TAME');
 exec dump_table_to_csv('TAMF');
+exec dump_table_to_csv('TBL9');
 quit;
